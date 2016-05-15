770ebce582ecfa69494115f251356c4bfed3de16
Victoria Catterson <vic@cowlet.org>
2016-05-15 11:32:52 +0100

    Pulse circles

--- d3-bearings.js
+++ d3-bearings.js
@@ -1,3 +1,26 @@
 (function () {
-  // stuff goes here
+  var circle = d3.selectAll("circle");
+
+  var min = 5;
+  var max = 15;
+  var delta = 0.1;
+
+  function step() {
+    var r = +circle.attr("r");
+
+    r = r + delta;
+
+    if (r > max) {
+      r = max;
+      delta = delta * -1;
+    } else if (r < min) {
+      r = min;
+      delta = delta * -1;
+    }
+
+    circle.attr("r", r);
+    requestAnimationFrame(step);
+  }
+
+  requestAnimationFrame(step);
 })();
