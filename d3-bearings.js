40c3c1957da62ff5bb429ebdea247c83dbdc9343
Victoria Catterson <vic@cowlet.org>
2016-05-15 12:29:03 +0100

    Plot two features against each other

--- d3-bearings.js
+++ d3-bearings.js
@@ -1,16 +1,38 @@
 (function () {
-  var data = [1, 3, 6, 18, 23];
-  var width = 720;
-  var height = 128;
+  d3.json("data/data.json", function(error, json) {
+    if (error) {
+      return console.warn(error);
+    }
+    visualise(json);
+  });
 
-  var vis = d3.select(".bearings")
-    .attr("width", width)
-    .attr("height", height);
 
-  var circle = vis.selectAll("circle")
-    .data(data)
-    .enter().append("circle")
-      .attr("cx", function(d, i) { return 40 * (i + 1); })
-      .attr("cy", 60)
-      .attr("r", function(d) { return d; });
+  function visualise(data) {
+    var width = 720;
+    var height = 720;
+
+    var vis = d3.select(".bearings")
+      .attr("width", width)
+      .attr("height", height);
+
+    var bpfi = data.map(function(d) { return d.bpfi; });
+    var bpfo = data.map(function(d) { return d.bpfo; });
+
+    var xscale = d3.scaleLinear()
+      .domain([d3.min(bpfi), d3.max(bpfi)])
+      .range([0, width])
+      .nice();
+
+    var yscale = d3.scaleLinear()
+      .domain([d3.min(bpfo), d3.max(bpfo)])
+      .range([height, 0])
+      .nice();
+
+    var circle = vis.selectAll("circle")
+      .data(data)
+      .enter().append("circle")
+        .attr("cx", function(d) { return xscale(d.bpfi); })
+        .attr("cy", function(d) { return yscale(d.bpfo); })
+        .attr("r", 5);
+  };
 })();
