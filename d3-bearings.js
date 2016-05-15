8fa66804b4dddffabb15de7fb9ccfc9a29024003
Victoria Catterson <vic@cowlet.org>
2016-05-15 14:57:24 +0100

    Add styled axes and transparent points

--- d3-bearings.js
+++ d3-bearings.js
@@ -8,13 +8,16 @@
 
 
   function visualise(data) {
-    var width = 580;
-    var height = 580;
+    var margin = {top: 20, right: 30, bottom: 30, left: 40};
+    var width = 580 - margin.left - margin.right;
+    var height = 580 - margin.top - margin.bottom;
     var radius = 4;
 
-    var vis = d3.select(".bearings")
-      .attr("width", width)
-      .attr("height", height);
+    var svg = d3.select(".bearings")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+    var vis = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var bpfi = data.map(function(d) { return d.bpfi; });
     var bpfo = data.map(function(d) { return d.bpfo; });
@@ -29,11 +32,24 @@
       .range([height - 2 * radius, 0 - radius])
       .nice();
 
+    var xaxis = d3.axisBottom(xscale);
+    svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+      .call(xaxis);
+
+    var yaxis = d3.axisLeft(yscale);
+    svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+      .call(yaxis);
+
     var circle = vis.selectAll("circle")
       .data(data)
       .enter().append("circle")
         .attr("cx", function(d) { return xscale(d.bpfi); })
         .attr("cy", function(d) { return yscale(d.bpfo); })
-        .attr("r", radius);
+        .attr("r", radius)
+        .style("fill", "rgba(0, 0, 0, 0.2)");
   };
 })();
